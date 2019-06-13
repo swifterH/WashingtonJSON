2def0f9d717b6083468ab8315a6532ad87ee9073
Micaela Cavallo <mlaracavallo@gmail.com>
2019-06-13 12:47:30 +0200

    Initial Commit

---
+++ WashingtonAPi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WashingtonAPi
+//
+//  Created by Micaela Cavallo on 6/13/19.
+//  Copyright © 2019 Micaela Cavallo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
